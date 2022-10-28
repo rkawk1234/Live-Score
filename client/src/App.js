206a3ce54a2a16a32a39d9922bdfcfb254ff36ef
rkawk1234 <okus6977@gmail.com>
2022-10-28 12:02:17 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,50 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link, Navigate } from 'react-router-dom';
 import Home from './components/home';
 import Login from './components/login';
 import Register from './components/register';
 import Blog from './components/blog';
-import {useState,useEffect} from 'react'
+// import {useState,useEffect} from 'react'
 import jwt_decode from 'jwt-decode'
 
 
 
 function App() {
   const token = localStorage.getItem('token')
-  const user = jwt_decode(token)
+  var user
+  // const [log,setLog] =  useState(token ? "" : "Login")
 
-  const [log,setLog] =  useState("Login")
-  useEffect(()=> {
+  if (token){
+    user = jwt_decode(token)
+
+  }
+  else{
+    user = {}
+  }
+  // const user = jwt_decode(token)
+
+
+  // const user = jwt_decode(token)
+
+ 
+  // useEffect(()=> {
     
-    if (token){
+  //   if (token){
       
-      setLog("Logout")
+  //     setLog("Logout")
 
-      // if (!user){
-      //   localStorage.removeItem('token')
-      //   navigate('/')
-      // } else{
+  //     // if (!user){
+  //     //   localStorage.removeItem('token')
+  //     //   navigate('/')
+  //     // } else{
 
         
 
-      // }
-    }
+  //     // }
+  //   }
 
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[])
+  //     // eslint-disable-next-line react-hooks/exhaustive-deps
+  // },[])
   return (
   
       <div className="App">
@@ -46,12 +59,18 @@ function App() {
             <Link to='/blog'>Blog</Link>
           </li>
           <li className='px-4 pb-2 pt-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-125 duration-200'>
-            <Link to='/login'>{log}</Link>
-          </li>
-          <li className='px-4 pb-2 pt-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-125 duration-200'>
             <Link to='/register'>Register</Link>
           </li>
-          <li className='mr-2'>User: {user.email}</li>
+          <li className='px-4 pb-2 pt-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-125 duration-200'>
+            <Link to='/login'>Login</Link>
+          </li>
+          
+          <li className='px-4 pb-2 pt-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-125 duration-200'
+              onClick={()=>{
+                localStorage.clear()
+              }}><Link to='/login'>Logout</Link></li>
+
+          <li className='mr-2'>User: {user ? user.email : ""}</li>
         </ul>
         <Routes>
           <Route exact path='/' element={<Home />} />
