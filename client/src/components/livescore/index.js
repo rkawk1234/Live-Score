831cffd98aa777a456c8efda083627a46f036532
rkawk1234 <okus6977@gmail.com>
2022-10-31 17:59:31 -0400

    livescore page updated rest api successful

--- client/src/components/livescore/index.js
+++ client/src/components/livescore/index.js
@@ -1,11 +1,15 @@
 import React from 'react'
 
+// import {useState} from 'react'
+
 import './index.css'
 
 
-const Livescore = () => {
 
-  var liveData
+const Livescore = () => {
+  // const [matchData,setMatchData] = useState({})
+  // const [team,setTeam] = useState({})
+
   const fetch = require('node-fetch');
 
   const url = 'https://api-football-v1.p.rapidapi.com/v3/fixtures?live=all&timezone=America%2FToronto';
@@ -19,8 +23,19 @@ const Livescore = () => {
   };
 
     fetch(url, options)
-    .then(res => res.json())
-    .then(json => console.log(json))
+    .then(res => 
+      res.json()
+    )
+    .then(json => {
+      console.log(json.response.length)
+
+      for (let i=0; i<json.response.length;i++){
+
+        document.getElementById("liveData").innerHTML += "<tr> <td>"+ json.response[i].teams.home.name +"</td> <td>"+ json.response[i].goals.home +"</td> <td>"+json.response[i].goals.away+" </td>  <td>"+ json.response[i].teams.away.name +"</td> </tr>"
+      }
+      
+
+})
     .catch(err => console.error('error:' + err));
   
 
@@ -35,6 +50,27 @@ const Livescore = () => {
 
   return (
     <div className='h-screen w-full bg-gradient-to-b from-stone-600 to-stone-300'>
+
+      <table id="liveTable"className='table-auto w-full text-center'>
+        <thead className="w-full">
+          <tr className="w-full  justify-between">
+            <th>Home</th>
+            
+            <th>Home Score</th>
+            <th>Away Score</th>
+
+            <th>Away</th>
+          </tr>
+        </thead>
+
+        <tbody id="liveData">
+
+
+        </tbody>
+
+
+      </table>
+
 
 
     </div>
