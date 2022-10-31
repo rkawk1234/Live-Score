d12b76824ad4cbf34438fa6e4a0907d47af0e25b
rkawk1234 <okus6977@gmail.com>
2022-10-31 14:41:02 -0400

    Update index.js

--- client/src/components/blog/index.js
+++ client/src/components/blog/index.js
@@ -6,8 +6,13 @@ import './index.css'
 const Blog = () => {
 
 
+
+
   return (
     <div className='h-screen w-full bg-gradient-to-b from-stone-600 to-stone-300'>
+
+      <div id="deck">
+      </div>
 
       <div className="Board">
         Board goes here
@@ -20,6 +25,8 @@ const Blog = () => {
       </div>
 
       <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Reveal Card</button>
+
+      
       
 
     </div>
