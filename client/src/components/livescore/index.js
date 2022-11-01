f4a156d4ce15fd7506545e3a64b3f2a5c3f6f0b1
rkawk1234 <okus6977@gmail.com>
2022-10-31 23:19:29 -0400

    past games, input made for different leagues

--- client/src/components/livescore/index.js
+++ client/src/components/livescore/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {useEffect} from 'react'
 
 // import {useState} from 'react'
 
@@ -9,39 +10,43 @@ import './index.css'
 const Livescore = () => {
   // const [matchData,setMatchData] = useState({})
   // const [team,setTeam] = useState({})
+  useEffect(() => {
+    const fetch = require('node-fetch');
 
-  const fetch = require('node-fetch');
-
-  const url = 'https://api-football-v1.p.rapidapi.com/v3/fixtures?live=all&timezone=America%2FToronto';
-
-  const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': 'a7bc8e7b5fmsh878701ce1dc6cb3p1d0d8fjsn07f3e8faa102',
-      'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
-    }
-  };
-
-    fetch(url, options)
-    .then(res => 
-      res.json()
-    )
-    .then(json => {
-      console.log(json.response.length)
-
-      for (let i=0; i<json.response.length;i++){
-
-        document.getElementById("liveData").innerHTML += "<tr> <td> <img src=" + json.response[i].teams.home.logo +"><img/>"+ json.response[i].teams.home.name +"</td> <td>"+ json.response[i].goals.home +"</td> <td>"+json.response[i].goals.away+" </td>  <td>"+ json.response[i].teams.away.name +"</td> </tr>"
+    const url = 'https://api-football-v1.p.rapidapi.com/v3/fixtures?live=all&timezone=America%2FToronto';
+  
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': 'a7bc8e7b5fmsh878701ce1dc6cb3p1d0d8fjsn07f3e8faa102',
+        'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
       }
-      
-
-})
-    .catch(err => console.error('error:' + err));
+    };
   
-
-
+    fetch(url, options)
+      .then(res =>
+        res.json()
+      )
+      .then(json => {
 
   
+        for (let i = 0; i < json.response.length; i++) {
+  
+          document.getElementById("liveData").innerHTML += "<tr> <td><img src=" + json.response[i].teams.home.logo + "><img/></td> <td>" + json.response[i].teams.home.name + "</td> <td>" + json.response[i].goals.home + "</td> <td>" + json.response[i].goals.away + " </td>  <td>" + json.response[i].teams.away.name + "</td>  <td> <img src=" + json.response[i].teams.away.logo + "><img/></td> </tr>"
+        }
+  
+  
+      })
+      .catch(err => console.error('error:' + err));
+
+  })
+
+ 
+
+
+
+
+
 
 
 
@@ -51,15 +56,17 @@ const Livescore = () => {
   return (
     <div className='h-screen w-full bg-gradient-to-b from-stone-600 to-stone-300'>
 
-      <table id="liveTable"className='table-auto w-full text-center'>
-        <thead className="w-full">
-          <tr className="w-full  justify-between">
+      <table id="liveTable" className="table-fixed w-full text-center border-separate border border-green-800">
+        <thead>
+          <tr >
+            <th></th>
             <th>Home</th>
-            
+
             <th>Home Score</th>
             <th>Away Score</th>
 
             <th>Away</th>
+            <th></th>
           </tr>
         </thead>
 
--- client/src/components/pastgames/index.js
+++ client/src/components/pastgames/index.js
@@ -1,22 +1,59 @@
 import React from 'react'
 import './index.css'
+// import { useEffect } from 'react';
+
 
 const PastGames = () => {
+
+
+
+
   return (
     <div className='h-screen w-full bg-gradient-to-b from-stone-900 to-stone-600'>
-      <form className="w-2/3">
-        <input type="text" id="league"></input>
-        <br/>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit" value="Submit">Click</button>
+
+
+      <form onSubmit={""} className=" flex flex-col shadow-md rounded px-8 pt-6 pb-8 mb-4 items-center">
+        <input type="text" className="w-1/5"></input>
+        <button
+          type="submit"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-1/12" >
+          Submit
+        </button>
 
       </form>
 
 
-      
+ 
+
+
+      <table id="liveTable" className="table-fixed w-full text-center border-separate border border-green-800">
+        <thead>
+          <tr >
+            <th></th>
+            <th>Home</th>
+
+            <th>Home Score</th>
+            <th>Away Score</th>
+
+            <th>Away</th>
+            <th></th>
+          </tr>
+        </thead>
+
+        <tbody id="liveData">
+
+
+        </tbody>
+
+
+      </table>
+
+
+
     </div>
 
 
-      
+
   )
 }
 
